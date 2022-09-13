36511cc35eac15bf88a0328dff27340780f20476
xB0SsZz <a93095@alunos.uminho.pt>
2022-09-13 21:31:54 +0100

    changed to append row if ALL args are in the cell and ignored case sensitivity

--- process.py
+++ process.py
@@ -6,6 +6,15 @@ import croped_delete
 
 
 def append_records(read_df, header, output_file, col, arg1, arg2, arg3, rows_to_delete):
+
+    args = list()
+    if arg1 != "NULL":
+        args.append(arg1)
+    if arg2 != "NULL":
+        args.append(arg2)
+    if arg2 != "NULL":
+        args.append(arg2)
+
     out_df = pd.DataFrame(columns=header)
     writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
     # in this block of code I check if there is a column with the name specified in the data_config_file.txt
@@ -22,7 +31,13 @@ def append_records(read_df, header, output_file, col, arg1, arg2, arg3, rows_to_
     # here starts the iteration through every row of example_croped.xlsx
     # I just explained what happens for one of the args, but it's the exact same procedure for the other two
     for j in range(0, len(read_df)):
-        if arg1 in str(read_df.iloc[j][read_df.columns[column]]):
+        flag = True
+        for a in range(0, len(args)):
+            if str(args[a]).lower() in str(read_df.iloc[j][read_df.columns[column]]).lower():
+                flag = True
+            else:
+                flag = False
+        if flag:
             # if arg1 is in row j and selected column, I store every value from this row in a list
             row = {}
             for k in range(0, len(read_df.columns)):
@@ -31,23 +46,7 @@ def append_records(read_df, header, output_file, col, arg1, arg2, arg3, rows_to_
             out_df = pd.concat([out_df, update_df], ignore_index=True)
             if j not in rows_to_delete:
                 rows_to_delete.append(j)
-        if arg2 in str(read_df.iloc[j][read_df.columns[column]]):
-            row = {}
-            for k in range(0, len(read_df.columns)):
-                row.update({f"{read_df.columns[k]}":read_df.iloc[j][read_df.columns[k]]})
-            update_df = pd.DataFrame(row, index=[0])
-            out_df = pd.concat([out_df, update_df], ignore_index=True)
-            if j not in rows_to_delete:
-                rows_to_delete.append(j)
-        if arg3 in str(read_df.iloc[j][read_df.columns[column]]):
-            row = {}
-            for k in range(0, len(read_df.columns)):
-                row.update({f"{read_df.columns[k]}":read_df.iloc[j][read_df.columns[k]]})
-            update_df = pd.DataFrame(row, index=[0])
-            out_df = pd.concat([out_df, update_df], ignore_index=True)
-            if j not in rows_to_delete:
-                rows_to_delete.append(j)
-
+        
     # when the output dataframe is complete, I save it using the writer I created before
     out_df.to_excel(writer, index=False, header=True)
     writer.save()
